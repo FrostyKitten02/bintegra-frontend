59b742ae54bcca90837c4afcfe3323416cfaf08c
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-20 21:22:02 +0200

    #3 - text fix

--- app/src/pages/MobilePlansPage.tsx
+++ app/src/pages/MobilePlansPage.tsx
@@ -7,17 +7,17 @@ export default function MobilePlansPage() {
             <h2 className="text-3xl font-bold text-center mb-12">Paketi</h2>
             <div className="grid lg:grid-cols-3 gap-6 lg:gap-x-12">
                 <PlanCard
-                    features={["10 GB mobilinih podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejene klici v Sloveniji"]}
+                    features={["10 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejene klici v Sloveniji"]}
                     name={"Osnovni paket"}
                     price={10}
                     priceInterval="mesec"/>
                 <PlanCard
-                    features={["20 GB mobilnih podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "5 GB mobilih podatkov v EU"]}
+                    features={["20 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "5 GB mobilih podatkov v EU"]}
                     name={"Srednji paket"}
                     price={12}
                     priceInterval="mesec"/>
                 <PlanCard
-                    features={["Neomejeni mobilni podatki", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "10 GB mobilih podatkov v EU"]}
+                    features={["Neomejen prenos podatki v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "10 GB mobilih podatkov v EU"]}
                     name={"Preminum paket"}
                     price={16}
                     priceInterval="mesec"/>
