ffeded1f3190c8ab47e39326f0fb160dabe1743b
klarakeb <klara.kebric@student.um.si>
2023-05-20 23:51:06 +0200

    #3 ADDED footer, EDITED navigation

--- app/src/template/NavigationBar.tsx
+++ app/src/template/NavigationBar.tsx
@@ -18,15 +18,17 @@ export default function NavigationBar() {
     const navigate = useNavigate();
     return(
         <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-6">
-            <div className="flex items-center flex-shrink-0 text-white mr-6 hover:cursor-pointer">
+            <div className="flex items-center flex-grow text-white mr-6 hover:cursor-pointer">
                 <a
                     className="font-semibold text-xl tracking-tight hover:text-gray-400"
                     onClick={()=>{navigate(Paths.HOME)}}
                 >Samopostrežni portal</a>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="w-full block lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
                     <NavItem name="Mobilini paketi" path={Paths.MOBILE_PLANS}/>
+                    <NavItem name={"Internet"} path={Paths.INTERNET_PLANS} />
+                    <NavItem name={"Televizija"} path={Paths.TV_PLANS} />
                 </div>
             </div>
         </nav>
