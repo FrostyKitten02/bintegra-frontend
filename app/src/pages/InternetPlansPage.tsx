1f3924d9feaaeb1200b4a48ea86f4d11e09f70e4
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-20 21:28:09 +0200

    #3 - internet plans

--- app/src/pages/InternetPlansPage.tsx
+++ app/src/pages/InternetPlansPage.tsx
@@ -1,4 +1,27 @@
+import PlanCard from "../components/PlanCard";
+
+
 export default function InternetPlansPage(){
-return (<>
-</>)
+    return (
+        <div className="mb-32 text-gray-800 grid justify-center pt-12">
+            <h2 className="text-3xl font-bold text-center mb-12">Izberite paket</h2>
+            <div className="grid lg:grid-cols-3 gap-6 lg:gap-x-12">
+                <PlanCard
+                    features={["Hitrosti do 100 mbps do uporabnika", "Hitrosti do 20 mbs od uporabnika"]}
+                    name={"Osnovni paket"}
+                    price={19.99}
+                    priceInterval="mesec"/>
+                <PlanCard
+                    features={["Hitrosti do 350 mbps do uporabnika", "Hitrosti do 80 mbs od uporabnika"]}
+                    name={"Srednji paket"}
+                    price={29.99}
+                    priceInterval="mesec"/>
+                <PlanCard
+                    features={["Hitrosti do 1 gbps do uporabnika", "Hitrosti do 200 mbs od uporabnika"]}
+                    name={"Preminum paket"}
+                    price={49.99}
+                    priceInterval="mesec"/>
+            </div>
+        </div>
+)
 }
--- app/src/pages/MobilePlansPage.tsx
+++ app/src/pages/MobilePlansPage.tsx
@@ -4,22 +4,22 @@ import PlanCard from "../components/PlanCard";
 export default function MobilePlansPage() {
     return (
         <div className="mb-32 text-gray-800 grid justify-center pt-12">
-            <h2 className="text-3xl font-bold text-center mb-12">Paketi</h2>
+            <h2 className="text-3xl font-bold text-center mb-12">Izberite paket</h2>
             <div className="grid lg:grid-cols-3 gap-6 lg:gap-x-12">
                 <PlanCard
                     features={["10 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejene klici v Sloveniji"]}
                     name={"Osnovni paket"}
-                    price={10}
+                    price={9.99}
                     priceInterval="mesec"/>
                 <PlanCard
                     features={["20 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "5 GB mobilih podatkov v EU"]}
                     name={"Srednji paket"}
-                    price={12}
+                    price={11.99}
                     priceInterval="mesec"/>
                 <PlanCard
                     features={["Neomejen prenos podatki v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "10 GB mobilih podatkov v EU"]}
                     name={"Preminum paket"}
-                    price={16}
+                    price={15.99}
                     priceInterval="mesec"/>
             </div>
         </div>
