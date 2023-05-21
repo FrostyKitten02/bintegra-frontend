8d4e78e6b1a7842589b22ec3ac64d5e91b7380a0
FrostyKitten02 <107218950+FrostyKitten02@users.noreply.github.com>
2023-05-21 15:07:24 +0200

    #4 - mobile and tv plans page (#2)

--- app/src/pages/TelevisionPlansPage.tsx
+++ app/src/pages/TelevisionPlansPage.tsx
@@ -1,4 +1,26 @@
+import PlanCard from "../components/PlanCard";
+
 export default function TelevisionPlansPage(){
-    return (<>
-    </>)
-    }
+    return  (
+        <div className="mb-32 text-gray-800 grid justify-center pt-12">
+            <h2 className="text-3xl font-bold text-center mb-12">Izberite paket</h2>
+            <div className="grid lg:grid-cols-3 gap-6 lg:gap-x-12">
+                <PlanCard
+                    features={["150+ TV programov", "Ogled nazaj do 7 dni"]}
+                    name={"Osnovni paket"}
+                    price={26.99}
+                    priceInterval="mesec"/>
+                <PlanCard
+                    features={["180+ TV programov", "Ogled nazaj do 7 dni"]}
+                    name={"Srednji paket"}
+                    price={34.99}
+                    priceInterval="mesec"/>
+                <PlanCard
+                    features={["230+ TV programov", "Ogled nazaj do 7 dni"]}
+                    name={"Preminum paket"}
+                    price={44.99}
+                    priceInterval="mesec"/>
+            </div>
+        </div>
+    )
+}
