c80e1ca04a42af8e5fb9742e9ed90a9d551f43d8
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-21 15:47:21 +0200

    #5 - sticky nav bar overlay

--- app/src/template/NavigationBar.tsx
+++ app/src/template/NavigationBar.tsx
@@ -17,7 +17,7 @@ const NavItem = ({name, path}:{name: string, path: string}) => {
 export default function NavigationBar() {
     const navigate = useNavigate();
     return(
-        <nav className="flex items-center sticky top-0 justify-center flex-wrap bg-gray-900">
+        <nav className="flex items-center sticky top-0 justify-center flex-wrap bg-gray-900 z-50">
             <div className="w-full block lg:flex lg:items-center lg:w-auto">
                 <div className="text-sm lg:flex-grow">
                     <NavItem name={"Mobilina telefonija"} path={Paths.MOBILE_PLANS}/>
