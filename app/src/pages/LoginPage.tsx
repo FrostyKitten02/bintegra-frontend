fc60fac9955e1cac0a0c96b711946f5058994d4d
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-21 16:06:22 +0200

    #6 - center loginpage

--- app/src/pages/LoginPage.tsx
+++ app/src/pages/LoginPage.tsx
@@ -24,7 +24,8 @@ export default function LoginPage() {
     }
 
     return(
-        <div className="mb-32 text-gray-800 grid justify-center pt-12">
+        <div className="mb-32 text-gray-800 grid justify-items-center pt-12">
+            <h2 className="text-3xl font-bold text-center mb-12">Prijava v samopostrežni portal</h2>
             <div className="w-full max-w-sm sm:min-w-300 md:min-w-400 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
                 <form className="space-y-6" onSubmit={handleSubmit}>
                     <h5 className="text-xl font-medium text-gray-900">Prijava</h5>
@@ -49,7 +50,7 @@ export default function LoginPage() {
                         <div className="flex items-start">
                             <div className="flex items-center h-5">
                                 <input id="remember" type="checkbox" value=""
-                                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+                                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 hover:cursor-pointer"
                                        required />
                             </div>
                             <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">Zapomni si me</label>
