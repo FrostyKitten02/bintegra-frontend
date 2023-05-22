5b49f0ad4750898c809a4a01efd736baba27730b
KLARA KIRBIS <klara.kirbis@student.um.si>
2023-05-22 22:42:40 +0200

    #13 - zbrisala ven zakomentirane stvari samo

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import { CheckIcon } from '@heroicons/react/20/solid'
 import React from "react";
-import PlanCard from "../components/PlanCard";
 
 const includedFeatures = [
     'Private forum access',
@@ -9,14 +8,7 @@ const includedFeatures = [
     'Official member t-shirt',
   ]
 export default function HomePage() {
-  {/*
-    const images = [
-      'https://media.idownloadblog.com/wp-content/uploads/2018/10/iPhone-XR-Ad-001.jpg',
-      'https://d39raawggeifpx.cloudfront.net/styles/16_9_desktop/s3/articleimages/bneCompanies-slovenia-TELEMACH-5G_800x450px.jpg',
-      'https://mloomnpztiey.i.optimole.com/w:1920/h:1080/q:mauto/f:avif/https://effektagency.com/wp-content/uploads/2018/09/telemach1-1.jpg',
-      // Add more image URLs here
-    ];*/
-  }
+
     return (
         <>
     {/* Nav */}
@@ -37,11 +29,6 @@ export default function HomePage() {
           </div>
         </div>
         <br/>
-        {/*<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-               d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"/>
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
-        */}
         {/*cards?*/}
         <div className="container">
           <div>
