615aee54f72de59204c0c4376beb967485344e57
klarakeb <klara.kebric@student.um.si>
2023-05-22 22:10:07 +0200

    #7 card look and transition

--- app/src/pages/MobilePlansPage.tsx
+++ app/src/pages/MobilePlansPage.tsx
@@ -1,30 +1,27 @@
 import PlanCard from "../components/PlanCard";
-import FancyPlanCard from "../components/FancyPlanCard";
 
 export default function MobilePlansPage() {
     return (
         <div className="mb-32 text-gray-800 grid justify-center pt-12">
-            <h2 className="text-3xl font-bold text-center mb-12">Izberite paket</h2>
-            <div className="grid mx-12 relative md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-x-16">
-                <FancyPlanCard
+            <h2 className="text-3xl font-bold text-center mb-12">
+                Izberite paket
+            </h2>
+            <div className="grid mx-12 relative md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 lg:gap-x-16">
+                <PlanCard
                     features={["10 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejene klici v Sloveniji"]}
                     name={"Osnovni paket"}
                     price={9.99}
                     priceInterval="mesec"/>
-                <FancyPlanCard
-                    features={["20 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "5 GB mobilih podatkov v EU"]}
+                <PlanCard
+                    features={["20 GB prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "5 GB mobilnih podatkov v EU"]}
                     name={"Srednji paket"}
                     price={11.99}
                     priceInterval="mesec"/>
-                {/*<PlanCard
-                    features={["Neomejen prenos podatki v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "10 GB mobilih podatkov v EU"]}
+                <PlanCard
+                    features={["Neomejeno število prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Sloveniji", "10 GB mobilnih podatkov v EU"]}
                     name={"Preminum paket"}
                     price={15.99}
-                    priceInterval="mesec"/>*/}
-                <FancyPlanCard features={["Neomejeno število prenosa podatkov v Sloveniji", "Neomejeni SMS-i", "Neomejeni klici v Slovneiji", "10 GB mobilih podatkov v EU"]}
-                               name={"Preminum paket"}
-                               price={15.99}
-                               priceInterval="mesec" />
+                    priceInterval="mesec"/>
             </div>
         </div>
     )
