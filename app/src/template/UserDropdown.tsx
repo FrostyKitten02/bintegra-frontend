15be679d71c626491eaf8c96a356775f22107dd1
KLARA KIRBIS <klara.kirbis@student.um.si>
2023-05-23 12:04:00 +0200

    #11 - userdropdown, linki do profila, sign/login

--- app/src/template/UserDropdown.tsx
+++ app/src/template/UserDropdown.tsx
@@ -1,43 +1,37 @@
 import {UserCircleIcon} from "@heroicons/react/24/outline";
 import {Dropdown} from "flowbite-react";
+import React from "react";
+import {Link} from "react-router-dom";
+import Paths from "../Paths";
 
 /*<UserCircleIcon className="transition ease-in-out hover:scale-110 duration-300 h-8 w-8 text-gray-500"/> */
 
 export default function UserDropdown() {
     return (
-        <Dropdown color={"white"}
-                  className={"z-50"}
-                  arrowIcon={false}
-                  trigger={"hover"}
-                  label={
-                      <>
-                          <UserCircleIcon className="transition ease-in-out hover:scale-110 duration-300 h-8 w-8 text-gray-500"/>
-                          <span className={"text-gray-500 px-2"}>
-                              Račun
-                          </span>
-                      </>
-                  }>
+        <Dropdown color="white" className="z-50" arrowIcon={false} trigger="hover" label={
+            <>
+                <UserCircleIcon className="transition ease-in-out hover:scale-110 duration-300 h-8 w-8 text-gray-500" />
+                <span className="text-gray-500 px-2">Račun</span>
+            </>
+        }>
             <Dropdown.Header>
-                <span className="block text-sm">
-                    Bonnie Green
-                </span>
-                <span className="block truncate text-sm font-medium">
-                    bonnie@flowbite.com
-                </span>
+                <span className="block text-sm">Bonnie Green</span>
+                <span className="block truncate text-sm font-medium">bonnie@flowbite.com</span>
             </Dropdown.Header>
             <Dropdown.Item>
-                Dashboard
+                <Link to={Paths.USER_PROFILE}>User Profile</Link>
             </Dropdown.Item>
             <Dropdown.Item>
-                Settings
+                <Link to={Paths.REGISTER}>Register</Link>
             </Dropdown.Item>
             <Dropdown.Item>
-                Earnings
+                <Link to={Paths.LOGIN}>Login</Link>
             </Dropdown.Item>
-            <Dropdown.Divider/>
-            <Dropdown.Item>
-                Sign out
-            </Dropdown.Item>
+            {/*<Dropdown.Item>
+                <Link to={Paths.EARNINGS}>Earnings</Link>
+            </Dropdown.Item>*/}
+            <Dropdown.Divider />
+
         </Dropdown>
     )
 }
