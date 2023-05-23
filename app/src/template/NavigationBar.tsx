8c3b0ee28d59f7436cd5bc524b09b460e1e5b38e
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-23 21:24:27 +0200

    #11 - navigation dropdown button

--- app/src/template/NavigationBar.tsx
+++ app/src/template/NavigationBar.tsx
@@ -20,37 +20,21 @@ const NavItem = ({name, path}: { name: string, path: string}) => {
 }
 
 export default function NavigationBar() {
-    const navigate = useNavigate();
     const [toggleMenuState, setToggleMenuState] = useState<boolean>(false);
 
-    const toggleMenu = (): void => {
-        setToggleMenuState(!toggleMenuState);
-    }
     return (
         <nav className="flex items-center sticky top-0 justify-center flex-wrap bg-gray-900 z-40">
-            <div className="hidden sm:block w-full lg:flex lg:items-center mx-12 lg:w-auto">
+            <button onClick={()=>{setToggleMenuState(prev=>!prev)}} className="text-white block lg:hidden w-full">
+                <Bars3Icon className="text-white h-10 ml-[72px]"/>
+            </button>
+
+            <div className={`${!toggleMenuState?"hidden":"block"} w-full lg:flex lg:items-center mx-12 lg:w-auto`} >
                 <div className="text-sm lg:flex-grow">
                     <NavItem name={"Mobilina telefonija"} path={Paths.MOBILE_PLANS}/>
                     <NavItem name={"Mobilni internet"} path={Paths.MOBILE_INTERNET_PLANS}/>
                     <NavItem name={"Internet"} path={Paths.INTERNET_PLANS}/>
                     <NavItem name={"Televizija"} path={Paths.TV_PLANS}/>
                 </div>
-            </div>
-            <div className="block w-full sm:hidden flex-grow text-white px-3 py-3">
-                <button className="click:bg-red-200 w-full">
-                    <Bars3Icon onClick={toggleMenu}
-                               className="h-6 w-6 text-white"/>
-                    {toggleMenuState &&
-                        <div className="sm:block">
-                            <div className="text-sm flex-grow">
-                                <NavItem name={"Mobilina telefonija"} path={Paths.MOBILE_PLANS}/>
-                                <NavItem name={"Mobilni internet"} path={Paths.MOBILE_INTERNET_PLANS}/>
-                                <NavItem name={"Internet"} path={Paths.INTERNET_PLANS}/>
-                                <NavItem name={"Televizija"} path={Paths.TV_PLANS}/>
-                            </div>
-                        </div>
-                    }
-                </button>
             </div>
         </nav>
     )
