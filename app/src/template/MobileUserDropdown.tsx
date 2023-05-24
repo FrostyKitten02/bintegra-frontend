a9c9cfaf0efc562971a0410415385835ab044d6e
kebklaferi <118739204+kebklaferi@users.noreply.github.com>
2023-05-24 17:58:11 +0200

    #7 and #16 navigation and card look (#5)
    * #7 card look and transition
    
    * #7 card look and transition
    
    * #16 ADDED navigation responsivnes
    
    * #11 - navigation dropdown button
    
    * #11 EDITED navigation
    
    * #16 EDITED responsive navigation
    
    ---------
    
    Co-authored-by: FrostyKitten02 <alen.fridau02@gmail.com>

---
+++ app/src/template/MobileUserDropdown.tsx
@@ -0,0 +1,39 @@
+import {UserCircleIcon} from "@heroicons/react/24/outline";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Paths from "../Paths";
+
+const UserDropdownItem = ({ name, path }: { name:string, path:string }) => {
+    const navigation = useNavigate();
+    return(
+        <li className = "text-right py-2 font-mono border-t border-t-gray-300">
+            <a className = " "
+               onClick={ () => navigation(path) }>
+                {name}
+            </a>
+        </li>
+    )
+}
+
+export default function MobileUserDropdown() {
+    const [dropdownState, setDropdownState] = useState<boolean>(false);
+
+    return (
+        <div>
+            <div className="flex justify-end pb-1">
+                <button onClick={() => setDropdownState(prevState => !prevState)} className="flex">
+                    <UserCircleIcon className="h-8 w-8 text-gray-500"/>
+                </button>
+            </div>
+            <div className={`${!dropdownState ? "hidden" : "block"}`}>
+                <div>
+                    <ul>
+                        <UserDropdownItem name="Prijava" path={Paths.LOGIN} />
+                        <UserDropdownItem name="Registracija" path={Paths.REGISTER} />
+                        <UserDropdownItem name="Profil" path={Paths.USER_PROFILE} />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
