15b4c3ebc77d784df4be66023c2198e87f96e4d9
klarakeb <klara.kebric@student.um.si>
2023-05-24 15:32:53 +0200

    #18 registration look

--- app/src/pages/RegisterPage.tsx
+++ app/src/pages/RegisterPage.tsx
@@ -30,63 +30,72 @@ export default function RegisterPage() {
 
     return(
         <div className="mb-32 text-gray-800 grid justify-items-center pt-12">
-            <h2 className="text-3xl font-bold text-center mb-12">Registracija v samopostrežni portal</h2>
-            <div
-                className="w-full max-w-[350px] sm:max-w-[450px] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <h5 className="text-xl font-medium text-gray-900">Registracija</h5>
-                    <InputWithLabel
-                        handleChange={handleChange}
-                        value={user.email??""}
-                        label="E-mail"
-                        name="email"
-                        type="email"
-                        placeholder="E-mail naslov"
-                        required
-                    />
+            <h2 className="text-3xl font-mono font-bold text-center mb-12">
+                Registracija v samopostrežni portal
+            </h2>
+            <div className="w-full flex justify-center">
+                <div className="z-60 bg-red-500 absolute w-[470px] h-96">
+                        hej
+                </div>
+                <div className="z-59 mt-5 ml-5 relative w-full max-w-[350px] hover:transition ease-in-out hover:duration-700 duration-700 hover:scale-105 sm:max-w-[450px] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
+                    <form className="space-y-6"
+                          onSubmit={handleSubmit}>
+                        <h5 className="text-xl font-mono font-medium text-gray-900">
+                            Registracija
+                        </h5>
+                        <InputWithLabel
+                            handleChange={handleChange}
+                            value={user.email??""}
+                            label="E-mail"
+                            name="email"
+                            type="email"
+                            placeholder="E-mail naslov"
+                            required
+                        />
 
-                    <InputWithLabel
-                        handleChange={handleChange}
-                        value={user.password??""}
-                        label="Geslo"
-                        name="password"
-                        type="password"
-                        placeholder="••••••••"
-                        event={passwordMissMatch?errorType:undefined}
-                        message={"Gesli se ne ujemata!"}
-                        required
-                    />
+                        <InputWithLabel
+                            handleChange={handleChange}
+                            value={user.password??""}
+                            label="Geslo"
+                            name="password"
+                            type="password"
+                            placeholder="••••••••"
+                            event={passwordMissMatch?errorType:undefined}
+                            message={"Gesli se ne ujemata!"}
+                            required
+                        />
 
-                    <InputWithLabel
-                        handleChange={handleChange}
-                        value={user.repeatPassword??""}
-                        label="Geslo"
-                        name="repeatPassword"
-                        type="password"
-                        placeholder="••••••••"
-                        event={passwordMissMatch?errorType:undefined}
-                        message={"Gesli se ne ujemata!"}
-                        required
-                    />
-
-                    <div className="flex items-start">
+                        <InputWithLabel
+                            handleChange={handleChange}
+                            value={user.repeatPassword??""}
+                            label="Geslo"
+                            name="repeatPassword"
+                            type="password"
+                            placeholder="••••••••"
+                            event={passwordMissMatch?errorType:undefined}
+                            message={"Gesli se ne ujemata!"}
+                            required
+                        />
                         <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                                <input id="remember" type="checkbox" value=""
-                                       className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 hover:cursor-pointer"
-                                       required/>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="remember" type="checkbox" value=""
+                                           className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 hover:cursor-pointer"
+                                           required/>
+                                </div>
+                                <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">
+                                    Strinjam se s <a className="ml-auto text-sm text-blue-700 hover:underline hover:cursor-pointer">pogoji poslovanja</a>
+                                </label>
                             </div>
-                            <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">
-                                Strinjam se s <a className="ml-auto text-sm text-blue-700 hover:underline hover:cursor-pointer">pogoji poslovanja</a>
-                            </label>
                         </div>
-                    </div>
 
-                    <button type="submit"
-                            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                        Registrirajte se
-                    </button>
-                </form>
+                        <button type="submit"
+                                className="w-full text-white bg-red-400 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            Registrirajte se
+                        </button>
+
+                    </form>
+                </div>
             </div>
         </div>
     )
