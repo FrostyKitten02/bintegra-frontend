e2fe2c29a21d0cf3426441be3f73682ed42a5d15
FrostyKitten02 <alen.fridau02@gmail.com>
2023-06-04 11:35:57 +0200

    #29 - client update

--- app/src/model/api/Client.tsx
+++ app/src/model/api/Client.tsx
@@ -1,14 +1,15 @@
 import {UserController} from "./controllers/UserController";
+import {OffersController} from "./controllers/OffersController";
 
 export class Client {
     private readonly authKey?: string;
 
-    // private readonly offersApi: OffersController;
+    private readonly offersApi: OffersController;
     public readonly UserApi: UserController;
 
     constructor(authKey?: string) {
         this.authKey = authKey;
-        // this.offersApi = new OffersController();
+        this.offersApi = new OffersController();
         this.UserApi = new UserController(authKey);
     }
 
